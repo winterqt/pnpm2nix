1bbc7da9084a98cc4958074fa27d9a6648bac79d
Winter <winter@winter.cafe>
2022-06-21 21:38:22 -0400

    Add support for reading lockfile version 5.3

--- default.nix
+++ default.nix
@@ -198,7 +198,7 @@ in {
       };
 
   in
-    assert (pnpmlock.lockfileVersion == 5 || pnpmlock.lockfileVersion == 5.1);
+    assert (pnpmlock.lockfileVersion == 5 || pnpmlock.lockfileVersion == 5.1 || pnpmlock.lockfileVersion == 5.2 || pnpmlock.lockfileVersion == 5.3);
   (mkPnpmDerivation {
     deps = (builtins.map
       (attrName: packages."${attrName}")
